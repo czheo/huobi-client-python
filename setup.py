aae9093ecd04a37be07f2e60eac95bea3da3b93d
czheo <czheo1987@gmail.com>
2017-01-01 14:44:00 -0800

    bump version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="huobi_client",
-    version="0.2.1",
+    version="0.3.0",
     description="a client library for huobi",
     author="czheo",
     license="LGPL",
